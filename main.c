15c99477d1aa4887dfaf956c9b71ad4e8c21f988
Mario Monteiro do Rosario <mmdorosario07@gmail.com>
2026-02-25 23:03:33 -0100

    just testing commit without paramethers

--- main.c
+++ main.c
@@ -4,6 +4,18 @@
 #include <time.h>
 
 int main() {
-	printf("Student-Helper");
+	unsigned short int exit_var;//get exit signal
+	printf("Student-Helper\n");
+	while (1){
+		printf("Press 0 to exit...\n");
+		scanf("%d", &exit_var);
+		if (exit_var == 0) {
+			printf("exit...\n");
+			exit(0);
+		} else {
+			printf("Please end this task!\n");
+		}
+	}
+
 	return 0;
 }
